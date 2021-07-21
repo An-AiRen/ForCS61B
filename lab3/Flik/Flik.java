d7969eca7a62be7c68ba57ff7263bcc81a77d2a5
Ai Ren <laowuti@outlook.com>
2021-07-21 14:25:16 +0800

    lab3 completed

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,9 @@
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
-    public static boolean isSameNumber(Integer a, Integer b) {
+    public static boolean isSameNumber(int a, int b) {
+        if (a != b) {
+            System.out.println(a == b);
+        }
         return a == b;
     }
 }
---
+++ lab3/Flik/FlikTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**To test Filk
+ * @author SumNull
+ * @Date   2021/7/21
+ */
+public class FlikTest {
+    @Test
+    public void testFlik() {
+        Integer a;
+        Integer b;
+
+        /** group one */
+        a = 0;
+        b = 0;
+        assertTrue(a == b);
+
+        /** group two */
+        a = 128;
+        b = 128;
+        assertTrue(a == b);
+    }
+
+}
