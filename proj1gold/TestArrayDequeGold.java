94ae993eb228c927400bd4e9eb8275dd61b16717
Ai Ren <laowuti@outlook.com>
2021-07-23 14:37:09 +0800

    complete proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -6,25 +6,32 @@ public class TestArrayDequeGold {
     public void testRandomlyCall() {
         StudentArrayDeque<Integer> sorry = new StudentArrayDeque<Integer>(); //actual one
         ArrayDequeSolution<Integer> nevermind = new ArrayDequeSolution<Integer>(); //expected one
-        double randomNumber;
+        int randomNumber;
         String messageSequence = "";
 
-        for (int i = 0; i < 60; i++) {
-            randomNumber = StdRandom.uniform();
-            if (randomNumber < 0.25) {
+        for (int i = 0; i < 500; i++) {
+            randomNumber = StdRandom.uniform(6);
+            if (randomNumber == 0) {
                 messageSequence += "addFirst(" + Integer.toString(i) + ")\n";
                 sorry.addFirst(i);
                 nevermind.addFirst(i);
-            } else if (randomNumber < 0.5) {
+            } else if (randomNumber == 1) {
                 messageSequence += "addLast(" + Integer.toString(i) + ")\n";
                 sorry.addLast(i);
                 nevermind.addLast(i);
-            } else if (randomNumber < 0.75 && sorry.size() > 0) {
+            } else if (randomNumber == 2 && sorry.size() > 0) {
                 messageSequence += "removeLast()\n";
                 assertEquals(messageSequence, nevermind.removeLast(), sorry.removeLast());
-            } else if (randomNumber < 1 && sorry.size() > 0) {
+            } else if (randomNumber == 3 && sorry.size() > 0) {
                 messageSequence += "removeFirst()\n";
                 assertEquals(messageSequence, nevermind.removeFirst(), sorry.removeFirst());
+            } else if (randomNumber == 4 && sorry.size() > 0) {
+                int ranIndex = StdRandom.uniform(sorry.size());
+                messageSequence += "get(" + Integer.toString(ranIndex) + ")\n";
+                assertEquals(messageSequence, nevermind.get(ranIndex), sorry.get(ranIndex));
+            } else {
+                messageSequence += "size()\n";
+                assertEquals(messageSequence, nevermind.size(), sorry.size());
             }
         }
     }
