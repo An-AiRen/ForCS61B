20747317ec174ca338542ebe2dfd2d36caa21795
Ai Ren <laowuti@outlook.com>
2021-08-03 16:29:35 +0800

    complete hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,124 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int gLen;
+    private int topS;
+    private int bottomS;
+    private int[][] sites;
+    private int openSites;
+    private WeightedQuickUnionUF possiblePath;
 
+    /** create N-by-N grid, with all sites initial */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        gLen = N;
+        topS = N * N;
+        bottomS = N * N + 1;
+        openSites = 0;
+
+        /** Using WQUUF to store the paths, the last two items serve as sentinel */
+        possiblePath = new WeightedQuickUnionUF(N * N + 2);
+
+        /** Performance requirements:N^2 */
+        sites = new int[N][N];
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                sites[i][j] = 0; //0 represent blocked, 1 open
+            }
+        }
+    }
+
+    /** Switch 2D to 1D in sequence*/
+    private int xyTo1D(int x, int y) {
+        return x * gLen + y;
+    }
+
+    /** Open the site(row, col) if it is not open */
+    public void open(int row, int col) {
+        /** Already opened, no need to reopen! */
+        if (sites[row][col] != 0) {
+            return;
+        }
+        sites[row][col] = 1;
+        merge(row, col);
+        openSites += 1;
+    }
+
+    /** is the site(row, col) open? */
+    public boolean isOpen(int row, int col) {
+        return sites[row][col] == 1;
+    }
+
+    /** is the site (row, col) full? */
+    public boolean isFull(int row, int col) {
+        return possiblePath.connected(xyTo1D(row, col), topS);
+    }
+
+    /** number of open sites */
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    /** does the system percolate? */
+    public boolean percolates() {
+        return possiblePath.connected(topS, bottomS);
+    }
+
+    /** Connect two sites according to rules. UGLY!*/
+    private void merge(int row, int col) {
+        int posIn1D = xyTo1D(row, col);
+        /** The first line */
+        if (row == 0) {
+            possiblePath.union(topS ,posIn1D);
+            /** Connect to the neighborhood */
+            if (isOpen(row + 1, col))
+                possiblePath.union(xyTo1D(row + 1, col), posIn1D);
+            if (col != 0 && isOpen(row, col - 1))
+                possiblePath.union(xyTo1D(row, col - 1), posIn1D);
+            if (col != gLen - 1 && isOpen(row, col + 1))
+                possiblePath.union(xyTo1D(row, col + 1), posIn1D);
+        } else if (row == gLen - 1) {
+            possiblePath.union(bottomS, posIn1D);
+
+            if (isOpen(row - 1, col))
+                possiblePath.union(xyTo1D(row - 1, col), posIn1D);
+            if (col != 0 && isOpen(row, col - 1))
+                possiblePath.union(xyTo1D(row, col - 1), posIn1D);
+            if (col != gLen - 1 && isOpen(row, col + 1))
+                possiblePath.union(xyTo1D(row, col + 1), posIn1D);
+        } else if (col == 0 || col == gLen - 1) {
+            if (isOpen(row - 1, col))
+                possiblePath.union(xyTo1D(row - 1, col), posIn1D);
+            if (isOpen(row + 1, col))
+                possiblePath.union(xyTo1D(row + 1, col), posIn1D);
+            if (col == 0 && isOpen(row, col + 1))
+                possiblePath.union(xyTo1D(row, col + 1), posIn1D);
+            if (col == gLen - 1 && isOpen(row, col - 1))
+                possiblePath.union(xyTo1D(row, col - 1), posIn1D);
+        } else {
+            if (isOpen(row - 1, col))
+                possiblePath.union(xyTo1D(row - 1, col), posIn1D);
+            if (isOpen(row + 1, col))
+                possiblePath.union(xyTo1D(row + 1, col), posIn1D);
+            if (isOpen(row, col - 1))
+                possiblePath.union(xyTo1D(row, col - 1), posIn1D);
+            if (isOpen(row, col + 1))
+                possiblePath.union(xyTo1D(row, col + 1), posIn1D);
+        }
+    }
+
+    public static void main(String[] args) {
+        Percolation p = new Percolation(4);
+
+        p.open(2, 3);
+        System.out.println(p.isOpen(2, 3));
+        p.open(1, 3);
+        p.open(1, 2);
+        p.open(0, 2);
+        p.open(3, 3);
+        System.out.println(p.percolates());
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,66 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
+import java.util.Random;
+
 public class PercolationStats {
+    private Percolation grid;
+    private double[] pThreshold;
+    private int pindex;
+
+    /** perform T independently */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0) {throw new IllegalArgumentException("N must greater than 0!");}
+        if (T <= 0) {throw new IllegalArgumentException("the experiment times must greater than 0!");}
+
+        pThreshold = new double[T];
+        pindex = 0;
+
+        for (int i = 0; i < T; i += 1) {
+            grid = pf.make(N);
+            StdRandom.setSeed(i);
+
+            while (!grid.percolates()) {
+                int x = StdRandom.uniform(0, N);
+                int y = StdRandom.uniform(0, N);
+                if (!grid.isOpen(x, y))
+                    grid.open(x, y);
+            }
+
+            pThreshold[pindex] = (double)grid.numberOfOpenSites() / (N * N);
+            pindex += 1;
+        }
+    }
+
+    /** sample mean of percolation threshold */
+    public double mean() {
+        double sum = 0;
+        for (double sample : pThreshold) {
+            sum += sample;
+        }
+        return sum /(double)pindex;
+    }
+
+    /** sample standard deviation of percolation threshold */
+    public double stddev() {
+        double sumSquare = 0;
+        double m = mean();
+        for (double sample : pThreshold) {
+            sumSquare += Math.pow(sample - m, 2);
+        }
+        return sumSquare / (double)(pindex - 1);
+    }
+
+    /** low endpoint of 95% confidence interval */
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(pindex);
+    }
+
+    /** high endpoint of 95% confidence interval */
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(pindex);
+    }
 
 }
